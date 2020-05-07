52d94ea8346844a9e4a78093979a9fbc515f69c8
LFRusso <lf.santos@usp.br>
2020-05-07 17:34:48 -0300

    Implementa toggleFlag, cumprindo o Desafio 1

--- Desafio 2/main.js
+++ Desafio 2/main.js
@@ -55,6 +55,15 @@ class Cell {
   // (This function is called inside cellRightClick function that are in the Map class,
   // you dont need to worry with that)
   // *************************************************************************************
+  toggleFlag(){
+	  this.isFlagged = !this.isFlagged;
+	  if(this.isFlagged){
+		  this.element.classList.add('flag');
+	  } else {
+		this.element.classList.remove('flag');
+	  }
+  }
+
 }
 
 class Map {
